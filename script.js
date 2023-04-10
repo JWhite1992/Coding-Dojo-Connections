13e386bad105e89afa65a85967a606ac89b3428a
JWhite1992 <121641404+JWhite1992@users.noreply.github.com>
2023-04-10 13:00:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function remove(element){
+    element.remove();
+}
+function change(name){
+    name.jane();
+}
+
+var h1= document.querySelector("h1");
+h1.innerText= "New Title";
+
+function switchh1(){
+    h1.change();
+}
+
